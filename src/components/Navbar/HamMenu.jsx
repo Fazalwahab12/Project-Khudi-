f85179f57e8d7d9b65671aecb29ef3636ce9799a
Fazalwahab12 <wahabdir567@gmail.com>
2024-05-02 03:43:39 -0700

    Sections Align

--- src/components/Navbar/HamMenu.jsx
+++ src/components/Navbar/HamMenu.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import "../Navbar/HamMenu.css"
-import { Link } from "react-router-dom"
+import React from "react";
+import "../Navbar/HamMenu.css";
+import { Link } from "react-router-dom";
 
 function HamMenu() {
   return (
@@ -11,13 +11,35 @@ function HamMenu() {
         <span></span>
         <span></span>
         <ul id="menu">
-          <Link to="home"><a href="#"><li>Home</li></a></Link>
-          <Link to="about"><a href="#"><li>About</li></a></Link>
-          <Link to="contact"><a href="#"><li>Contact</li></a></Link>
+          <Link to="home">
+            <a href="#">
+              <li>Hero</li>
+            </a>
+          </Link>
+          <Link to="about">
+            <a href="#">
+              <li>About us</li>
+            </a>
+          </Link>
+          <Link to="contact">
+            <a href="#">
+              <li>Campaigns</li>
+            </a>
+          </Link>
+          <Link to="about">
+            <a href="#">
+              <li>Meet Our Warriors</li>
+            </a>
+          </Link>
+          <Link to="contact">
+            <a href="#">
+              <li>Testimonials</li>
+            </a>
+          </Link>
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default HamMenu
+export default HamMenu;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,57 +1,52 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import CharitioLogo from "../Images/CharitioLogo.png"
-import { BiSearch } from "react-icons/bi"
-import "../Navbar/Navbar.css"
-import ScrollToTop from './scrollToTop'
-import HamMenu from "./HamMenu"
+import React from "react";
+import { Link } from "react-router-dom";
+import CharitioLogo from "../Images/CharitioLogo.png";
+import { BiSearch } from "react-icons/bi";
+import "../Navbar/Navbar.css";
+import ScrollToTop from "./scrollToTop";
+import HamMenu from "./HamMenu";
+
 function Navbar() {
-    return (
-        <nav className='navbar'>
-            <div className='navbar-container'>
-                <div className='ham-menu'>
-                    < HamMenu />
-                </div>
-                <div className='img-container'>
-                    <img src={CharitioLogo} id="charitioLogo" alt="" />
-                </div>
-                <div className='center-buttons'>
-                    <div class="dropdown">
-                        <button className='dropbtn'> <Link to="home"> Home </Link> </button>
-                        <div class="dropdown-content">
-                            <ul className='dropdown-content-navbar'>
-                                <li className='hover-underline-animation'>Home Charity</li>
-                                <li className='hover-underline-animation'>Home Education</li>
-                                <li className='hover-underline-animation'>Home Wildlife</li>
-                                <li className='hover-underline-animation'>Home Ocean Polution</li>
-                                <li className='hover-underline-animation'>Home World Pandemic</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <button>
-                        <Link to="about">
-                            About
-                        </Link>
-                    </button>
-                    <button><Link to="contact">
-                        Contact
-                    </Link>
-                    </button>
-                </div>
-                <div className='right-buttons'>
-                    <button id='donate-btn'><Link to="contact">
-                        Donate Now
-                    </Link>
-                    </button>
-                    <button id='search-nav'>
-                        <Link to="contact">
-                            <BiSearch />
-                        </Link>
-                    </button>
-                </div>
-            </div>
-            < ScrollToTop />
-        </nav>
-    )
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="ham-menu">
+          <HamMenu />
+        </div>
+        <div className="img-container">
+          <img src={CharitioLogo} id="charitioLogo" alt="" />
+        </div>
+        <div className="center-buttons">
+          <button>
+            <Link to="Hero">Hero</Link>
+          </button>
+          <button>
+            <Link to="about">About us</Link>
+          </button>
+          <button>
+            <Link to="Campaigns"> Campaigns</Link>
+          </button>
+          <button>
+            <Link to="Warriors"> Meet Our Warriors</Link>
+          </button>
+          <button>
+            <Link to="Testimonials">Testimonials</Link>
+          </button>
+        </div>
+        <div className="right-buttons">
+          <button id="donate-btn">
+            <Link to="contact">Donate Now</Link>
+          </button>
+          <button id="search-nav">
+            <Link to="contact">
+              <BiSearch />
+            </Link>
+          </button>
+        </div>
+      </div>
+      <ScrollToTop />
+    </nav>
+  );
 }
-export default Navbar
+
+export default Navbar;
