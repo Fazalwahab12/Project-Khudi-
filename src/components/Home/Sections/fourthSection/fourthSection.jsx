d97bad7feebe16a69ab2e85cda3c88f738f84b87
Fazalwahab12 <wahabdir567@gmail.com>
2024-05-24 06:25:48 -0700

    div image

--- src/components/Home/Sections/fourthSection/fourthSection.jsx
+++ src/components/Home/Sections/fourthSection/fourthSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FourthMedicine from "../../../Images/Campaigns1.jpeg";
-import Food from "../../../Images/food.png";
-import Education from "../../../Images/education.png";
+import compagin2 from "../../../Images/compagin2.jpeg";
+import Education from "../../../Images/compagin3.jpeg";
 const fourthSection = () => {
   return (
     <div id="campaigns" className="">
@@ -18,8 +18,8 @@ const fourthSection = () => {
         </div>
         <div className="  ">
           <div className="flex  md:mx-16 mt-6  gap-10 flex-col  md:flex-row">
-            <div className=" shadow-lg rounded-2xl">
-              <img src={Education} className="rounded-3xl p-2" />
+            <div className=" shadow-lg rounded-2xl  ">
+              <img src={compagin2} className="rounded-3xl p-2 " height={50} />
               <br />
               <h2 className=" p-3">
                 <a href="#">
@@ -39,7 +39,7 @@ const fourthSection = () => {
               </h2>
             </div>
             <div className="shadow-lg rounded-2xl">
-              <img src={FourthMedicine} className=" rounded-3xl p-2" />
+              <img src={compagin2} className=" rounded-3xl p-2" />
               <br />
               <h2 className=" p-3">
                 <a href="#">
